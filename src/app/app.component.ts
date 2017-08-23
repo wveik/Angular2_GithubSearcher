b5a2f52e1a422cffe937a5e2a19041a5af585604
wveik <kataev_06@list.ru>
2017-08-23 17:14:49 +0300

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<profile></profile>'
+})
+export class AppComponent {
+  title = 'app';
+}
---
+++ src/app/components/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'profile',
+    templateUrl: './profile.component.html',
+  //styleUrls: ['./app.component.css']
+})
+export class ProfileComponent {
+  title = 'app';
+}
